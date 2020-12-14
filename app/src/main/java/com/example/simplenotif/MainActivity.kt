86553277d78896c03788fc6341933bd7b37384ca
Juanda Esa <juandaesaputra@gmail.com>
2020-12-14 12:42:26 +0700

    add pending intent

--- app/src/main/java/com/example/simplenotif/MainActivity.kt
+++ app/src/main/java/com/example/simplenotif/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.simplenotif
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
@@ -20,11 +24,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    //aksi untuk onClick pada button
+    fun sendNotification(view: View) {
+
+        //Digunakan untuk memberikan action jika notifikasi disentuh
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"))
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         // Kode ini digunakan untuk menambahkan elemen apa saja yang akan ditampikan
         // pada notifikasi seperti icon, action, title, content, dan lain-lain.
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentIntent(pendingIntent)
                 .setSmallIcon(R.drawable.ic_notifications)
                 .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications))
                 .setContentTitle(resources.getString(R.string.content_title) )
@@ -47,6 +60,6 @@ class MainActivity : AppCompatActivity() {
         val notification = mBuilder.build()
 
         mNotificationManager.notify(NOTIFICATION_ID, notification)
-
     }
+    
 }
